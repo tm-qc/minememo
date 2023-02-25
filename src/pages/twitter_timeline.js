6757c8387f6df5164697d64e180d54705b37d39d
tm-qc <web.tm.mail@gmail.com>
2023-02-25 14:54:41 +0900

    雑レスポンシブ修正
    適当に流してたところ全部返ってくる（　＾ω＾）・・・

--- src/pages/twitter_timeline.js
+++ src/pages/twitter_timeline.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Timeline } from 'react-twitter-widgets';
 import { useStaticQuery, graphql } from "gatsby"
 
+// CSS読み込み
+import * as style from '../components/css/twitter.module.css';
+
 const TwitterTimeLine = () => {
   const data = useStaticQuery(graphql`
     query{
@@ -17,6 +20,7 @@ const TwitterTimeLine = () => {
   // console.log(data,"TwitterTimeline")
   return (
       <main>
+        <div className={style.twitterFrame}>
         <Timeline
           dataSource={{
             sourceType: 'profile',
@@ -26,6 +30,7 @@ const TwitterTimeLine = () => {
             height: '600'
           }}
         />
+        </div>
       </main>
   )
 }
