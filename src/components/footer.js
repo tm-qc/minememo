b5097d00008f495d9b440704e421727d05dc3ada
tm-qc <web.tm.mail@gmail.com>
2023-02-26 17:23:04 +0900

    header footer 調整

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 // useStaticQuery, graphqlも使うので読み込む
-import { useStaticQuery, graphql } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 // CSS読み込み(カレントディレクトリで解決すべきリクエストは、'./' で始める必要があるとのこと)
 import * as style from './css/footer.module.css';
 
@@ -19,7 +19,7 @@ const Footer = () => {
   );
   return (
     <footer>
-      <div>Copyright © {data.site.siteMetadata.startYear} - {new Date().getFullYear()} {data.site.siteMetadata.title} </div>
+      <div><Link to="/">Copyright © {data.site.siteMetadata.startYear} - {new Date().getFullYear()} {data.site.siteMetadata.title} </Link></div>
     </footer>
   );
 };
